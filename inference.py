e3bd4e7b27208f6964ba1c9bef7249c1e75ed3c0
lnewmanjohnson <lnewmanjohnson@expression.net>
2017-11-17 16:17:28 -0500

    finally figured out what was going wrong in the ObserveState. I feel dumb.

--- inference.py
+++ inference.py
@@ -450,6 +450,7 @@ class JointParticleFilter:
                     self.particleList.append(state)
                     i += 1
 
+
     def addGhostAgent(self, agent):
         """
         Each ghost agent is registered separately and stored (in case they are
@@ -464,26 +465,20 @@ class JointParticleFilter:
         """
         Resamples the set of particles using the likelihood of the noisy
         observations.
-
         To loop over the ghosts, use:
-
           for i in range(self.numGhosts):
             ...
-
         A correct implementation will handle two special cases:
           1) When a ghost is captured by Pacman, all particles should be updated
              so that the ghost appears in its prison cell, position
              self.getJailPosition(i) where `i` is the index of the ghost.
-
              As before, you can check if a ghost has been captured by Pacman by
              checking if it has a noisyDistance of None.
-
           2) When all particles receive 0 weight, they should be recreated from
              the prior distribution by calling initializeParticles. After all
              particles are generated randomly, any ghosts that are eaten (have
-             noisyDistance of None) must be changed to the jail Position. This
-             will involve changing each particle if a ghost has been eaten.
-
+             noisyDistance of 0) must be changed to the jail Position. This will
+             involve changing each particle if a ghost has been eaten.
         self.getParticleWithGhostInJail is a helper method to edit a specific
         particle. Since we store particles as tuples, they must be converted to
         a list, edited, and then converted back to a tuple. This is a common
@@ -496,81 +491,68 @@ class JointParticleFilter:
         emissionModels = [busters.getObservationDistribution(dist) for dist in noisyDistances]
 
         "*** YOUR CODE HERE ***"
-        #acquire persistent objects
-        possibleStates = list(itertools.product(self.legalPositions, self.legalPositions))
-
-        #eat test
-        #this is the first way to do it by manipulating the distribution
         
+        #eat test
         for j in range(self.numGhosts):
             if (noisyDistances[j] == None):
                 i = 0
                 while (i < len(self.particleList)):
                     if (self.particleList[i][j] != self.getJailPosition(j)):
-                        print("before:",self.particleList[i], self.getJailPosition(j))
                         self.particleList[i] = self.getParticleWithGhostInJail(self.particleList[i], j)
-                        print("after:",self.particleList[i])
                     i += 1
         distribution = self.getBeliefDistribution()
-                        
 
-        """
-        while (i < len(self.particleList)):
+        #reweight distrubtion based on new information
+        for state in distribution:
             for j in range(self.numGhosts):
-                if (noisyDistances[j] == None):
-                    self.particleList[i] = self.getParticleWithGhostInJail(self.particleList[i], j)
-            i += 1
-
-
-
-        
-        for i in range(self.numGhosts):
-            if (noisyDistances[i] == None):
-                print("ghost",i, " is dead")
-                print("distribution before:",distribution)
-                for state in possibleStates:
-                    if (state[i] != self.getJailPosition(i)):
-                        distribution[self.getJailPosition(i)] += distribution[state]
-                        distribution[state] = 0
-                    distribution.normalize()
-                print("distribution after:",distribution)
-        
-
-        """
-        """
-        if not (all(distance != None for distance in noisyDistances)):
-            print("in eatTest")
-            for i in range(self.numGhosts):
-                if (noisyDistances[i] == None):
-                    print("ghost ",i, " is eaten")
-                    for state in possibleStates:
-                        if (state[i] != self.getJailPosition(i)):
-                            #print(self.getJailPosition(i))
-                            distribution[self.getJailPosition(i)] += distribution[state]
-                            distribution[state] = 0
-        distribution.normalize()
-        """
-
-        #there may be another way to do it by manipulating the particleList            
-        
-        for state in possibleStates:
-            for i in range(self.numGhosts):
-                distance = util.manhattanDistance(state[i], pacmanPosition)
-                distribution[state] = distribution[state] * emissionModels[i][distance]
-            distribution.normalize() 
+                if (noisyDistances[j] != None):
+                    distribution[state] = distribution[state]*emissionModels[j][util.manhattanDistance(state[j], pacmanPosition)]
 
         #test for bottoming out
         if (distribution.totalCount() == 0):
             self.initializeParticles()
             distribution = self.getBeliefDistribution()
-        distribution.normalize()
 
         #resample for next iteration
         self.particleList = []
         i = 0
         while (i < self.numParticles):
             self.particleList.append(util.sample(distribution))
-            i += 1    
+            i += 1
+
+        """
+
+        #eat test
+        if (noisyDistance == None):
+            distribution = util.Counter()
+            distribution[self.getJailPosition()] = 1
+            distribution.normalize() #TODO
+            self.particleList = []
+            i = 0
+            while (i < self.numParticles):
+                self.particleList.append(util.sample(distribution))
+                i += 1
+
+        #reweight distrubtion based on new information
+        for state in self.legalPositions:
+            distanceToPacman = util.manhattanDistance(state, pacmanPosition)
+            distribution[state] = distribution[state] * emissionModel[distanceToPacman]
+        distribution.normalize() #TODO
+
+        #test for bottoming out
+        if (distribution.totalCount() == 0):
+            self.initializeUniformly(gameState)
+            distribution = self.getBeliefDistribution()
+        distribution.normalize() #TODO
+
+        #resample for next iteration
+        self.particleList = []
+        i = 0
+        while (i < self.numParticles):
+            self.particleList.append(util.sample(distribution))
+            i += 1        
+        """
+
 
     def getParticleWithGhostInJail(self, particle, ghostIndex):
         """
